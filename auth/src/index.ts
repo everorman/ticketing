55ad1987f0b73ab18364c7c0d96e51caa38ae840
Evert Ortiz <evert.ortiz@encora.com>
2023-04-13 16:40:34 -0500

    Adding an error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { currentUserRouter } from './router/current-user';
 import { signinRouter } from './router/singin';
 import { signoutRouter } from './router/singout';
 import { signupRouter } from './router/singup';
+import { errorHandler } from './moddlewares/error-handler';
 
 const app = express();
 app.use(json());
@@ -12,7 +13,7 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
-
+app.use(errorHandler);
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
 });
--- auth/src/router/singup.ts
+++ auth/src/router/singup.ts
@@ -16,9 +16,9 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new Error('Invalid email and password');
     }
-    const { email, password } = req.body();
+    const { email, password } = req.body;
     res.send({});
   }
 );
