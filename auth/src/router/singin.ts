a12647a21c9bbb37741e6a37e4e4087df3c8a33f
Evert Ortiz <evert.ortiz.m@gmail.com>
2023-05-03 18:32:45 -0500

    Adding middleware to check req body

--- auth/src/router/singin.ts
+++ auth/src/router/singin.ts
@@ -1,9 +1,19 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { validateRequest } from '../middlewares/validate-request';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hi there signin');
+router.post('/api/users/signin', [
+  body('email')
+    .isEmail()
+    .withMessage('You must provide a valid email'),
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('You must provide a password')
+], validateRequest, (req: Request, res: Response) => {
+
 });
 
 export { router as signinRouter };
