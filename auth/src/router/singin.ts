9d5a1f57e3ef50938ab77c8e091b8c38bfdb61ad
Evert Ortiz <evert.ortiz.m@gmail.com>
2023-05-04 10:28:08 -0500

    Adding singin method

--- auth/src/router/singin.ts
+++ auth/src/router/singin.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
 import { validateRequest } from '../middlewares/validate-request';
 import { body } from 'express-validator';
-
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
+import jwt from 'jsonwebtoken';
 const router = express.Router();
 
 router.post('/api/users/signin', [
@@ -12,8 +15,30 @@ router.post('/api/users/signin', [
     .trim()
     .notEmpty()
     .withMessage('You must provide a password')
-], validateRequest, (req: Request, res: Response) => {
+],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
 
-});
+    const existingUser = await User.findOne({ email })
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordMatch = await Password.toCompare(existingUser.password, password);
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  });
 
 export { router as signinRouter };
