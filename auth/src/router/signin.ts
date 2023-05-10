9196cd66dea28eac2f3953fdb16a405082811487
Evert Ortiz <evert.ortiz.m@gmail.com>
2023-05-10 12:45:21 -0500

    fix bug in signin method

--- auth/src/router/signin.ts
+++ auth/src/router/signin.ts
@@ -7,38 +7,51 @@ import { Password } from '../services/password';
 import jwt from 'jsonwebtoken';
 const router = express.Router();
 
-router.post('/api/users/signin', [
-  body('email')
-    .isEmail()
-    .withMessage('You must provide a valid email'),
-  body('password')
-    .trim()
-    .notEmpty()
-    .withMessage('You must provide a password')
-],
-  validateRequest,
-  async (req: Request, res: Response) => {
-    const { email, password } = req.body;
+router.post(
+    '/api/users/signin',
+    [
+        body('email')
+            .isEmail()
+            .withMessage('You must provide a valid email'),
+        body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('You must provide a password'),
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
 
-    const existingUser = await User.findOne({ email })
-    if (!existingUser) {
-      throw new BadRequestError('Invalid credentials');
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            throw new BadRequestError(
+                'Invalid credentials'
+            );
+        }
+
+        const passwordMatch = await Password.toCompare(
+            existingUser.password,
+            password
+        );
+        if (!passwordMatch) {
+            throw new BadRequestError(
+                'Invalid credentials'
+            );
+        }
+        const userJwt = jwt.sign(
+            {
+                id: existingUser.id,
+                email: existingUser.email,
+            },
+            process.env.JWT_KEY!
+        );
+
+        req.session = {
+            jwt: userJwt,
+        };
+
+        res.status(200).send(existingUser);
     }
-
-    const passwordMatch = await Password.toCompare(existingUser.password, password);
-    const userJwt = jwt.sign(
-      {
-        id: existingUser.id,
-        email: existingUser.email,
-      },
-      process.env.JWT_KEY!
-    );
-
-    req.session = {
-      jwt: userJwt,
-    };
-
-    res.status(200).send(existingUser);
-  });
+);
 
 export { router as signinRouter };
